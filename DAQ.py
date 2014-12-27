09f560e438eb948aff9362ab89d47131142a4a4c
Maziar Saleh Ziabari <helasraizam@gmail.com>
2014-12-27 17:44:54 -0600

    fixed things, code-related.

--- DAQ.py
+++ DAQ.py
@@ -6,11 +6,22 @@ from math import *
 import numpy as np
 import serial
 
+data=[]
+
 def readSerial():
     ser=serial.Serial('/dev/ttyACM0',9600,timeout=1)
-    while True:
-        txt=ser.readline()
+    print 'Ignoring data to start at beginning.'
+    while txt!='':
+        pass
+    print 'Paused data.'
+    while txt=='':
+        pass
+    print 'Gathering data.'
+    while txt!='':
+        data.append(ser.readline().split('\t'))
         print 'Nothing.' if txt=='' else txt
+    print 'Done, closing port.'
     ser.close()
 
+print data
 readSerial()
