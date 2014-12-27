85c563b12f6fee754381ddd3a22fe78fe91c8476
bromid <salehziabari@mx.lakeforest.edu>
2014-12-27 16:51:00 -0600

    Ard script called ard.ino get it?Helasraizam didn't

---
+++ ard.ino
@@ -0,0 +1,27 @@
+int outPin = 13;      // sets amplifier input
+int inPin = A0;       // Read amplifier output (analog)
+float inVoltage = 0;  // True input voltage
+float outVoltage = 0; // True output voltage
+
+void setup()
+{
+  Serial.begin(9600);        // Connect to serial 9600
+  pinMode(outPin, OUTPUT);   // Set outPin to write
+  pinMode(inPin, INPUT);     // Set inPin to read
+}
+
+void loop()
+{
+  for(int inValue=0;inValue<=255;inValue++)  // loop through all values 0-255
+  {
+    analogWrite(outPin,inValue);  // set amplifier input to inValue
+    inVoltage=inValue*5.0/255;    // Convert to true voltage
+    Serial.print(inVoltage);      // print true voltage of amplifier IN
+    delay(100);                   // Give it .1 s to stabilize
+    
+    outVoltage = 5.0/1023*analogRead(inPin);   // Get amplifier OUT
+    Serial.print('\t');
+    Serial.println(outVoltage);
+  }
+  delay(10000);                    // Wait 10s between readings
+}
